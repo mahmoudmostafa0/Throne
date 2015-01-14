9831f024d87b763da2dc949eb9b6d8e2423efa19
Anthrax- <hoda002009@hotmail.com>
2015-01-14 19:41:20 +0200

    Update [1101] Map Item Info.cs
    Added Item Pickup

--- Projects/Services/Throne.World/Network/Messages/[1101] Map Item Info.cs
+++ Projects/Services/Throne.World/Network/Messages/[1101] Map Item Info.cs
@@ -1,11 +1,24 @@
-﻿using System;
+using System;
+using Throne.Framework.Network.Connectivity;
 using Throne.Framework.Network.Transmission;
+using Throne.Framework.Security.Permissions;
 using Throne.World.Structures.Objects;
 
 namespace Throne.World.Network.Messages
 {
+    [Handling.WorldPacketHandler(PacketTypes.MapItem, Permission = typeof(AuthenticatedPermission))]
     public sealed class MapItemInformation : WorldPacket
     {
+        public MapItemInformation(byte[] payload)
+            : base(payload)
+        {
+        }
+        public int Timestamp;
+        public uint UID, ItemID;
+        public ushort X, Y;
+        public Item.Color Color;
+        public MapItemTypes Type;
+        private Character Character;
         public enum MapItemTypes : short
         {
             None,
@@ -27,7 +40,32 @@ namespace Throne.World.Network.Messages
             DaggerStormV3,
             DaggerStorm = 46
         }
-
+        public override bool Read(IClient client)
+        {
+            Timestamp = ReadInt();
+            UID = ReadUInt();
+            ItemID = ReadUInt();
+            X = ReadUShort();
+            Y = ReadUShort();
+            Color = (Item.Color)ReadUShort();
+            Type = (MapItemTypes)ReadUShort();
+            return true;
+        }
+        public override void Handle(IClient client)
+        {
+            Character = ((WorldClient)client).Character;
+            if (Character.Location.Position.X == X && Character.Location.Position.Y == Y) //it should check if char is trading too.
+            {
+                if (!Character.AdequateInventorySpace(1))
+                    return;
+                var map = Character.Location.Map;
+                var item = map.GetItem(UID);
+                if (item == null) return;
+                item.OwnerInfo = Character.Record;
+                map.RemoveItem(item);
+                Character.MoveToInventory(item);
+            }
+        }
         public MapItemInformation(Item item, Boolean remove = false) : base(PacketTypes.MapItem, 48)
         {
             WriteInt(Environment.TickCount);
